cf4d5a54cedde4a8bf33df5097634637458ab952
OlagunjuOlaniyi <olagunjuolaniyi512@gmail.com>
2022-07-20 11:09:04 +0100

    Creating Sub Hero across pages

---
+++ src/components/SubHero/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const SubHeroCt = styled.div`
+  background-color: var(--olacommerce-bg);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 130px;
+  color: #fff;
+  font-size: 30px;
+  font-weight: 700;
+  margin-top: 70px;
+
+  @media screen and (max-width: 780px) {
+    margin-top: 50px;
+  }
+`;
