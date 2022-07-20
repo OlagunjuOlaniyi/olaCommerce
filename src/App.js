21e40dfbc96879a9be3a586c987586ede5cb09b0
OlagunjuOlaniyi <olagunjuolaniyi512@gmail.com>
2022-07-20 11:13:42 +0100

    Updating App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Header from "./components/Header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
+import Shop from "./pages/Shop/Shop";
+import Contact from "./pages/Contact/Contact";
+import Cart from "./pages/Cart/Cart";
+import Checkout from "./pages/Checkout/Checkout";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
         <Footer />
       </BrowserRouter>
