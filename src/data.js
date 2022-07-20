35ebbf9d4e9d01e36e7fd2d2b5dc5e145e46fd20
OlagunjuOlaniyi <olagunjuolaniyi512@gmail.com>
2022-07-20 11:14:38 +0100

    creating data component for test product

---
+++ src/data.js
@@ -0,0 +1,9 @@
+import product from "../src/images/product.jpg";
+export const products = [
+  {
+    id: 1,
+    img: product,
+    description: "lorem ipsun loren ipsum",
+    price: 99,
+  },
+];
