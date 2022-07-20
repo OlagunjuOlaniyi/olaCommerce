98593b0ed973c0c43e50912f91b5ec9f64642f47
OlagunjuOlaniyi <olagunjuolaniyi512@gmail.com>
2022-07-20 11:12:18 +0100

    Developing Contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import { SubHeroCt } from '../../components/SubHero/style'
+import React from "react";
+import Form from "../../components/Form/Form";
+import { SubHeroCt } from "../../components/SubHero/style";
 
 const Contact = () => {
   return (
     <div>
-        <SubHeroCt>Contact</SubHeroCt>
+      <SubHeroCt>Contact Us</SubHeroCt>
+      <main className="my-3">
+        <Form />
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
